996f4fb20a2fc7df2cf1587c0905b3320c59c51f
Code Sandbox <hello@codesandbox.io>
2021-08-06 15:24:06 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.09668258b92bad2a29917b628c29cf81.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f925683e237b18a05ae7ed970eaeb5",
+    "url": "https://nabanitabag.github.io/csb-74t54/index.html"
+  },
+  {
+    "revision": "dd733091157731fab647",
+    "url": "https://nabanitabag.github.io/csb-74t54/static/js/2.4a5b600f.chunk.js"
+  },
+  {
+    "revision": "7709b98cb3d1f316e343",
+    "url": "https://nabanitabag.github.io/csb-74t54/static/js/main.8faa9497.chunk.js"
+  },
+  {
+    "revision": "b823e6e56183aec9eefd",
+    "url": "https://nabanitabag.github.io/csb-74t54/static/js/runtime-main.2d61d425.js"
+  }
+]);
